d5fc9fbf87abc2d0e5633cc3568c18a21c5c18e8
suprajaelecharala <113628359+suprajaelecharala@users.noreply.github.com>
2023-10-25 19:51:20 -0400

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/funfood/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/funfood/ExampleInstrumentedTest.java
@@ -20,7 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        //Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.funfood", appContext.getPackageName());
     }
 }
